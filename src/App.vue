a7804fcc4ff14d496f7f513aa3ced74483a309ec
valyy151 <marin.valenta1@gmail.com>
2023-02-17 17:01:01 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<Navbar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	import Navbar from './components/Vue-Navbar.vue';
+	export default {
+		name: 'App',
+		components: { Navbar },
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 </style>
---
+++ src/components/Vue-Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+	<header>
+		<div class="logo"><a href="">App</a></div>
+		<div class="links">
+			<a href="">Home</a>
+			<a href="">About</a>
+			<a href="">Support</a>
+			<a href="">Login</a>
+		</div>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: 'Vue-Navbar',
+	};
+</script>
+
+<style scoped>
+	header {
+		display: flex;
+		background-color: #3eaf7c;
+		padding: 10px 0;
+	}
+	header a {
+		color: white;
+		text-decoration: none;
+		font-size: 1.3em;
+	}
+	.logo {
+		margin: 0 auto 0 35px;
+	}
+
+	.links {
+		display: flex;
+		justify-content: space-evenly;
+		width: 20%;
+	}
+</style>
